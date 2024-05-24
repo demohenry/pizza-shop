8767bdf15af6fc2b21d5b8ed0444918460d33c86
Democrito <demohenry6@gmail.com>
2024-05-24 17:05:53 -0300

    utilizando a lib sonner para notificações toast

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,6 +1,7 @@
 import { Label } from "@radix-ui/react-label";
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
@@ -22,6 +23,18 @@ export function SignIn() {
   async function handleSignIn(data: SignInForm) {
     console.log(data);
     await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    toast.success(
+      "Enviamos um link de autenticação para o e-mail cadastrado.",
+      {
+        action: {
+          label: "Reeviar",
+          onClick: () => {
+            handleSignIn(data);
+          },
+        },
+      },
+    );
   }
 
   return (
