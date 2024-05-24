98842a15c552f8f564eb8aeb04676e6239379245
Democrito <demohenry6@gmail.com>
2024-05-24 16:26:49 -0300

    Criação da pagina de login & instalação de componentes adicionais shadcn-ui

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,4 +1,8 @@
+import { Label } from "@radix-ui/react-label";
 import { Helmet } from "react-helmet-async";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export function SignIn() {
   return (
@@ -7,9 +11,25 @@ export function SignIn() {
       <div className="p-8">
         <h1>SignIn</h1>
         <div className="w-[358px] flex flex-col justify-center gap-6">
-          <div></div>
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar painel
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Acompanhe suas vendas pelo painel do parceiro!
+            </p>
+          </div>
 
-          <div></div>
+          <form className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu e-mail</Label>
+              <Input id="email" type="email" />
+            </div>
+
+            <Button className="w-full" type="submit">
+              Acessar painel
+            </Button>
+          </form>
         </div>
       </div>
     </>
