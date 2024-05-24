162046657f09f0f256be6b00419a66c9804b2050
Democrito <demohenry6@gmail.com>
2024-05-24 15:56:56 -0300

    Setup inicial do Projeto

---
+++ prettier.config.cjs
@@ -0,0 +1,4 @@
+/* eslint-disable no-undef */
+module.modules = {
+  "plugins": ["prettier-plugin-tailwindcss"]
+}
---
+++ src/pages/auth/sign-in.tsx
@@ -0,0 +1,17 @@
+import { Helmet } from "react-helmet-async";
+
+export function SignIn() {
+  return (
+    <>
+      <Helmet title="Login" />
+      <div className="p-8">
+        <h1>SignIn</h1>
+        <div className="w-[358px] flex flex-col justify-center gap-6">
+          <div></div>
+
+          <div></div>
+        </div>
+      </div>
+    </>
+  );
+}
