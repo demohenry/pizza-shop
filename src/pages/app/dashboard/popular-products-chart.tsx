dd9e42318e5fc816a4e88264d80c7503fa6b8e39
Democrito <demohenry6@gmail.com>
2024-06-03 16:16:29 -0300

    Criando página de NotFound para rotas não encontradas

--- src/pages/app/dashboard/popular-products-chart.tsx
+++ src/pages/app/dashboard/popular-products-chart.tsx
@@ -1,14 +1,8 @@
 import { BarChart } from "lucide-react";
-import { Cell, Pie, PieChart, ResponsiveContainer, Tooltip } from "recharts";
+import { Cell, Pie, PieChart, ResponsiveContainer } from "recharts";
 import colors from "tailwindcss/colors";
 
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 const COLORS = [
   colors.sky[500],
