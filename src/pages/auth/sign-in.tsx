659a7f42b27228d263a42c736b5331468054567e
Democrito <demohenry6@gmail.com>
2024-06-10 18:10:49 -0300

    Alteração em auth para receber useMutation criada

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,12 @@
 import { Label } from "@radix-ui/react-label";
+import { useMutation } from "@tanstack/react-query";
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import { toast } from "sonner";
 import { z } from "zod";
 
+import { signIn } from "@/api/sign-in";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
@@ -21,21 +23,28 @@ export function SignIn() {
     formState: { isSubmitting },
   } = useForm<SignInForm>();
 
-  async function handleSignIn(data: SignInForm) {
-    console.log(data);
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+  const { mutateAsync: authenticate } = useMutation({
+    mutationFn: signIn,
+  });
 
-    toast.success(
-      "Enviamos um link de autenticação para o e-mail cadastrado.",
-      {
-        action: {
-          label: "Reeviar",
-          onClick: () => {
-            handleSignIn(data);
+  async function handleSignIn(data: SignInForm) {
+    try {
+      await authenticate({ email: data.email });
+
+      toast.success(
+        "Enviamos um link de autenticação para o e-mail cadastrado.",
+        {
+          action: {
+            label: "Reeviar",
+            onClick: () => {
+              handleSignIn(data);
+            },
           },
         },
-      },
-    );
+      );
+    } catch {
+      toast.error("Credencials invalidos");
+    }
   }
 
   return (
