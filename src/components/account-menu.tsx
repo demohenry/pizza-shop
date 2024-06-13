d80c1cc7c6917392327c652e27854aed97f84a46
Democrito <demohenry6@gmail.com>
2024-06-13 14:42:11 -0300

    Criação de Loading state com react-query

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -14,25 +14,31 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
+import { Skeleton } from "./ui/skeleton";
 
 export function AccountMenu() {
   const [isOpen, setIsOpen] = useState(false);
 
-  const { data: profile } = useQuery({
+  const { data: profile, isLoading: IsLoadingProfile } = useQuery({
     queryKey: ["profile"],
     queryFn: getProfile,
   });
 
-  const { data: managedRestaurant } = useQuery({
-    queryKey: ["managed-restaurant"],
-    queryFn: getManagedRestaurant,
-  });
+  const { data: managedRestaurant, isLoading: IsLoadingManagedRestaurant } =
+    useQuery({
+      queryKey: ["managed-restaurant"],
+      queryFn: getManagedRestaurant,
+    });
 
   return (
     <DropdownMenu onOpenChange={setIsOpen}>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" className="flex items-center gap-2">
-          {managedRestaurant?.name}
+          {IsLoadingManagedRestaurant ? (
+            <Skeleton className="h-4 w-40" />
+          ) : (
+            managedRestaurant?.name
+          )}
           <ChevronDown
             className={`h-4 w-4 transition-transform duration-200 ${isOpen ? "-rotate-180" : ""}`}
           />
@@ -40,10 +46,19 @@ export function AccountMenu() {
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="w-56">
         <DropdownMenuLabel className="flex flex-col ">
-          <span>{profile?.name}</span>
-          <span className="text-xs font-normal text-muted-foreground">
-            {profile?.email}
-          </span>
+          {IsLoadingProfile ? (
+            <div className="space-y-1.5">
+              <Skeleton className="h-4 w-32" />
+              <Skeleton className="h-3 w-24" />
+            </div>
+          ) : (
+            <>
+              <span>{profile?.name}</span>
+              <span className="text-xs font-normal text-muted-foreground">
+                {profile?.email}
+              </span>
+            </>
+          )}
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuItem className="cursor-pointer">
